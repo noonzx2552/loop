5356e8fe9aa40b3350e27e54bc6af8436ef1aebc
noonzx2552 <noonzx2552@gmail.com>
2023-09-28 23:47:20 +0700

    Add existing file

--- B4.py
+++ B4.py
@@ -1,2 +1,37 @@
+import random
+cake = int(input("howmuchcakedoyouwant?:"))
 
- 
+chocolate = {
+    "แป้ง": 1,
+    "น้ำตาล": 0.5,
+    "ไข่ไก่": 3
+}
+
+strawberry = {
+    "แป้ง": 1,
+    "น้ำตาล": 1,
+    "ไข่ไก่": 2
+}
+
+orange = {
+    "แป้ง": 1,
+    "น้ำตาล": 4,  
+    "ไข่ไก่": 2,
+    "น้ำส้ม": 0.25
+}
+
+sugar_red = 0
+
+shoose = random.choice(["ช็อคโกแลต", "สตอเบอรี่", "ส้ม"])
+for i in range(cake)
+        if shoose == "ช็อกโกเเลต":
+                print("")
+        elif shoose == "สตอเบอรี่":
+                print("")
+        else:
+
+
+
+if sugar > 4:
+        print("sugar-red:",sugar - 4)
+else:
